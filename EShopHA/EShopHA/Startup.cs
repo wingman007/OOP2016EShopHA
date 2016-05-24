855d301d834631fadfb31a6982a65c17a1c821c1
angelbgpl <angelbgpl@abv.bg>
2016-05-24 16:36:45 +0300

    Start Application

---
+++ EShopHA/EShopHA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EShopHA.Startup))]
+namespace EShopHA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
