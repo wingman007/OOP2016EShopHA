15f5b500a92df69aa9bfd02c3d4cdde249b78bcd
SkyNet <SkyNet@SKYNET1>
2016-06-02 21:59:17 +0300

    Category and Products- Classes and Database created
    Category and Products- Classes and Database created. Admin User: admin;
    pass: admin123

---
+++ EShopHA/EShopHA/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EShopHA.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+    }
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> products { get; set; }
+    }
+}
